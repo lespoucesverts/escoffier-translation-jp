231d1f9dda9f3daa8fc4ff59e7b9998e5e5f3e3d
manabu <manabu@iMacManabu.local>
2018-05-07 18:36:48 +0900

    20180507-18h36

--- Docs/dotemacs-first/init.el
+++ Docs/dotemacs-first/init.el
@@ -1,6 +1,5 @@
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("melpa" . "http://melpa.org/packages/")))
-;;        ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 ;;(init-loader-load)
