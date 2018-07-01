c4d7c7af88c9fe13fe6207da633228cba06cc6fb
五島 学 <dottore@lespoucesverts.org>
2018-07-01 13:30:42 +0900

    20180701-13h30

--- chapitres/07-releves-et-entrees/auto/07-00.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "07-00"
- (lambda ()
-   (TeX-run-style-hooks
-    "07-releves-et-entrees/07-01-pp393-414"))
- :latex)
-
--- chapitres/07-releves-et-entrees/auto/07-01-pp393-414.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "07-01-pp393-414"
- (lambda ()
-   (LaTeX-add-labels
-    "vii.ux8089ux6599ux7406-relevuxe9s-et-entruxe9es"))
- :latex)
-
